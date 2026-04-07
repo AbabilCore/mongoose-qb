ccf032e627d7ea00df60c6b23217b1f812f0bf7f
AbabilCore <ababilspark@gmail.com>
2026-04-07 20:54:15 +0600

    chore: remove biome linter and simplify build tooling

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export * from "./provider";
+export type {
+  IQBConfig,
+  IQBMeta,
+  IUseQueryOptions,
+  IUseQueryResult,
+  IQBPopulate,
+} from "./core/types";
+export { createQuery } from "./core";
+export { useQuery } from "./utils";
--- src/provider.ts
+++
@@ -1,9 +0,0 @@
-export type {
-  IQBConfig,
-  IQBMeta,
-  IUseQueryOptions,
-  IUseQueryResult,
-  IQBPopulate,
-} from "./core/types";
-export { createQuery } from "./core";
-export { useQuery } from "./utils";
